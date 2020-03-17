49440e2f0dbf73fc50d94a2d17202ba3b9b04c55
Lukas Zanner <git@lukaszanner.de>
2020-03-17 22:11:05 +0100

    Make some progress in the initial window setup

--- init.el
+++ init.el
@@ -1,16 +1,43 @@
-; New buffers are in text mode
-(setq-default major-mode 'text-mode)
+;;-----------------------------------------------------------------;;
+;; LOAD LIBRARIES
+;;-----------------------------------------------------------------;;
+;; Load all needed libraries first
+;;-----------------------------------------------------------------;;
 
-; Turn off the toolbar
+
+
+;;-----------------------------------------------------------------;;
+;; GLOBAL CONFIG
+;;-----------------------------------------------------------------;;
+;; Various global settings
+;;-----------------------------------------------------------------;;
+
+;; Get rid of all those bars
 (tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
 
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
+;; Colors
+(set-foreground-color "burlywood3")
+(set-background-color "#161616")
+(set-cursor-color "#40FF40")
 
-(defun post-load-stuff ()
-  (interactive)
-  (menu-bar-mode -1)
-  (set-foreground-color "burlywood3")
-  (set-background-color "#161616")
-  (set-cursor-color "#40FF40")
-)
-(add-hook 'window-setup-hook 'post-load-stuff t)
+;; Line numbers
+(column-number-mode t)
+(global-display-line-numbers-mode t)
+
+;; Setup default and initial frame
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+(add-to-list 'default-frame-alist '(foreground-color . "burlywood3"))
+(add-to-list 'default-frame-alist '(background-color .  "#161616"))
+(add-to-list 'default-frame-alist '(cursor-color . "#40FF40"))
+
+;; Start the initial frame split into two windows
+(split-window-horizontally)
+
+;;-----------------------------------------------------------------;;
+;;-----------------------------------------------------------------;;
+;;-----------------------------------------------------------------;;
+
+;; New buffers are in text mode
+(setq-default major-mode 'text-mode)

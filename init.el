edb0c6fadf0c73ac5d3b3d13ac6b58d43e8ac29f
Lukas Zanner <git@lukaszanner.de>
2020-03-17 22:52:40 +0100

    Add melpa package available

--- init.el
+++ init.el
@@ -4,7 +4,16 @@
 ;; Load all needed libraries first
 ;;-----------------------------------------------------------------;;
 
+;; Load emacs 24's package system. Add MELPA repository.
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   ;; '("melpa" . "http://stable.melpa.org/packages/") ; many packages won't show if using stable
+   '("melpa" . "http://melpa.milkbox.net/packages/")
+   t))
 
+(package-initialize)
 
 ;;-----------------------------------------------------------------;;
 ;; GLOBAL CONFIG
